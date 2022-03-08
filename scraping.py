61168bb55a71bca596d829126e3eba110a352920
alimakouar <makouarali@gmail.com>
2022-03-08 21:48:06 +0100

    importing the libraries and setting the headers

---
+++ scraping.py
@@ -0,0 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"}
